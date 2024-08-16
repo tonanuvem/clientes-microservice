66c994ad03df8771036083be97605f1c45a20208
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-08-16 00:42:36 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker build -t imagem_clientes_microservice .
-docker run --name clientes-microservice -it -p 5050:5000 -d imagem_clientes_microservice
+docker run --rm --name clientes-microservice -it -p 5050:5000 -d imagem_clientes_microservice
 
 SERVER_IP=$(curl checkip.amazonaws.com)
 echo ""
